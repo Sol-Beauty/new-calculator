44ce3f993d06092415542714335d26a64689e75a
thedevofthedevil666 <gabriel.verduzco@solbeautyandcare.com>
2022-06-01 09:05:47 -0700

    The old calculator was refactored, and the type of request data was modified to be as well the Size-Calculator(the original)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-
 import numpy
 from fastapi import FastAPI, status, APIRouter, HTTPException
 from pydantic import BaseModel
@@ -11,7 +10,7 @@ app = FastAPI(
     title="FastAPI Pydiator",
     description="FastAPI pydiator integration project",
     version="1.0.0",
-   # root_path="/predictor",
+    # root_path="/predictor",
 )
 
 app.add_middleware(
@@ -31,6 +30,7 @@ class Size(BaseModel):
     hips: float
     measure_type: str
 
+
 @app.get("/", status_code=200)
 def read_root():
     return {"Hello": "World"}
@@ -47,41 +47,80 @@ def get_new_size(size: Size):
     match size.type:
         # For the new one
         case 0:
-            df = pd.read_csv('/app/new_jeans_dataset.csv')
-            WaistCm = df['WaistCm'].to_numpy()
-            HipsCm = df['HipsCm'].to_numpy()
-            indexesWaist = np.where(WaistCm == size.waist)
-            index = indexesWaist[0][0]
-            resultHips = HipsCm[index]
-            print(resultHips)
-            if resultHips == size.hips:
-                jeans = df['NewJeans'].to_numpy()
-                sizeFinal = jeans[index]
-                print(sizeFinal)
-                return sizeFinal
-            else:
-                raise HTTPException(status_code=400, detail="La talla no existe, favor de comunicarse con algún asesor")
-                return False
+            if size.measure_type == "cm":
 
+                df = pd.read_csv('/app/new_jeans_dataset.csv')
+                WaistCm = df['WaistCm'].to_numpy()
+                HipsCm = df['HipsCm'].to_numpy()
+                indexesWaist = np.where(WaistCm == size.waist)
+                index = indexesWaist[0][0]
+                resultHips = HipsCm[index]
+                print(resultHips)
+                if resultHips == size.hips:
+                    jeans = df['NewJeans'].to_numpy()
+                    sizeFinal = jeans[index]
+                    print(sizeFinal)
+                    return sizeFinal
+                else:
+                    raise HTTPException(status_code=400, detail="La talla no existe, favor de comunicarse con algún asesor")
+
+            if size.measure_type == "in":
+                df = pd.read_csv('new_jeans_dataset.csv')
+                WaistIn = df['WaistIn'].to_numpy()
+                HipsIn = df['HipsIn'].to_numpy()
+                indexesWaistIn = np.where(WaistIn == size.waist)
+                print(size.waist)
+                print(size.hips)
+                print("Indexes WaistIn", indexesWaistIn)
+                indexIn = indexesWaistIn[0]
+                resultHips = HipsIn[indexIn]
+                print("WaistIn", WaistIn)
+                print("HipsIn", HipsIn)
+                if resultHips == size.hips:
+                    jeans = df['NewJeans'].to_numpy()
+                    sizeFinal = jeans[indexIn]
+                    print(sizeFinal)
+                    return sizeFinal
+                else:
+                    raise HTTPException(status_code=400, detail="La talla no existe, favor de comunicarse con algún "
+                                                                "asesor")
         case 1:
-            df = pd.read_csv('new_jeans_dataset.csv')
-            WaistIn = df['WaistIn'].to_numpy()
-            HipsIn = df['HipsIn'].to_numpy()
-            indexesWaistIn = np.where(WaistIn == size.waist)
-            print(size.waist)
-            print(size.hips)
-            print("Indexes WaistIn", indexesWaistIn)
-            indexIn = indexesWaistIn[0]
-            resultHips = HipsIn[indexIn]
-            print("WaistIn", WaistIn)
-            print("HipsIn", HipsIn)
-            if resultHips == size.hips:
-                jeans = df['NewJeans'].to_numpy()
-                sizeFinal = jeans[indexIn]
-                print(sizeFinal)
-                return sizeFinal
-            else:
-                raise HTTPException(status_code=400, detail="La talla no existe, favor de comunicarse con algún asesor")
-                return False
+            if size.measure_type == "cm":
+                df = pd.read_csv('/app/new_jeans_dataset.csv')
+                WaistCm = df['WaistCm'].to_numpy()
+                HipsCm = df['HipsCm'].to_numpy()
+                indexesWaist = np.where(WaistCm == size.waist)
+                index = indexesWaist[0][0]
+                resultHips = HipsCm[index]
+                print(resultHips)
+                if resultHips == size.hips:
+                    jeans = df['JeansSBC'].to_numpy()
+                    sizeFinal = jeans[index]
+                    print(sizeFinal)
+                    return sizeFinal
+                else:
+                    raise HTTPException(status_code=400,
+                                        detail="La talla no existe, favor de comunicarse con algún asesor")
+
+            if size.measure_type == "in":
+                df = pd.read_csv('new_jeans_dataset.csv')
+                WaistIn = df['WaistIn'].to_numpy()
+                HipsIn = df['HipsIn'].to_numpy()
+                indexesWaistIn = np.where(WaistIn == size.waist)
+                print(size.waist)
+                print(size.hips)
+                print("Indexes WaistIn", indexesWaistIn)
+                indexIn = indexesWaistIn[0]
+                resultHips = HipsIn[indexIn]
+                print("WaistIn", WaistIn)
+                print("HipsIn", HipsIn)
+                if resultHips == size.hips:
+                    jeans = df['JeansSBC'].to_numpy()
+                    sizeFinal = jeans[indexIn]
+                    print(sizeFinal)
+                    return sizeFinal
+                else:
+                    raise HTTPException(status_code=400, detail="La talla no existe, favor de comunicarse con algún "
+                                                                "asesor")
         case default:
             raise HTTPException(status_code=404, detail="No hay o existe una opcion por favor elija una para continuar")
