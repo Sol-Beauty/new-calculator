b2f154ee0ae67e4dce33e8641521537ccf9a65f6
Kenneth Betancourt <kenneth_betancourt@solbeautyandcare.com>
2022-06-02 11:49:31 -0700

    calculator

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ app = FastAPI(
     title="FastAPI Pydiator",
     description="FastAPI pydiator integration project",
     version="1.0.0",
-    root_path="/predictor",
+    root_path="/calculator",
 )
 
 app.add_middleware(
